6d7b1cf2b673c873a7776314c36ab73ff4ac0280
lillie <onlinechronically@protonmail.com>
2025-07-04 16:29:36 -0400

    add: Postgres feature (db connection)

--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,10 @@
-fn main() {}
+use dotenv::dotenv;
+use sqlx::postgres::PgPoolOptions;
+
+#[tokio::main]
+async fn main() -> Result<(), sqlx::Error> {
+    dotenv().ok();
+    let db = std::env::var("DATABASE_URL").expect("Missing Database URL");
+    let pool = PgPoolOptions::new().max_connections(5).connect(&db).await?;
+    Ok(())
+}
