81d3c9ae683fa0297cc44d27905419d6b3905d2b
lillie <onlinechronically@protonmail.com>
2025-07-04 17:15:30 -0400

    add: Discord Library+Placeholder Code

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,53 @@
 use dotenv::dotenv;
+use poise::serenity_prelude as serenity;
 use sqlx::postgres::PgPoolOptions;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+enum Error {
+    #[error("PostgreSQL Error")]
+    Postgres(#[from] sqlx::Error),
+    #[error("Discord Error")]
+    Discord(#[from] serenity::Error),
+}
+
+// Required Struct for poise
+struct Data {}
+
+/// Says something
+#[poise::command(slash_command, prefix_command)]
+async fn knock(
+    ctx: poise::Context<'_, Data, Box<dyn std::error::Error + Send + Sync>>,
+) -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
+    ctx.say("hello world :3").await?;
+    Ok(())
+}
 
 #[tokio::main]
 async fn main() -> Result<(), sqlx::Error> {
     dotenv().ok();
     let db = std::env::var("DATABASE_URL").expect("Missing Database URL");
+    let token = std::env::var("DISCORD_TOKEN").expect("Missing Discord Token");
+
+    let client_intents = serenity::GatewayIntents::non_privileged();
+    let client_framework = poise::Framework::builder()
+        .options(poise::FrameworkOptions {
+            commands: vec![knock()],
+            ..Default::default()
+        })
+        .setup(|ctx, _, framework| {
+            Box::pin(async move {
+                poise::builtins::register_globally(ctx, &framework.options().commands).await?;
+                Ok(Data {})
+            })
+        })
+        .build();
+
     let pool = PgPoolOptions::new().max_connections(5).connect(&db).await?;
+    let mut client = serenity::ClientBuilder::new(token, client_intents)
+        .framework(client_framework)
+        .await?;
+
+    client.start().await?;
     Ok(())
 }
